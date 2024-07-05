333274bf9917a68bc39ccdc41d6c85f9ec79e40a
Ubuntu <ubuntu@ip-172-31-20-246.us-east-2.compute.internal>
2024-07-05 16:18:09 +0000

    added allowed host in setting.py file

--- django-project/devops_project/devops_project/settings.py
+++ django-project/devops_project/devops_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j^&jrq_6y7!zje%zpmfm7e@7qux7nmn&u715sutqvq5hd)l-#%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [18.219.199.223]
 
 
 # Application definition
